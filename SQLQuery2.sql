76da2f02eaa28900e8ddcea62357292a45db9b35
Gavin Swofford <gavinswofford@gmail.com>
2020-09-01 09:03:47 -0500

    finished up to practice six

---
+++ SQLQuery2.sql
@@ -0,0 +1,35 @@
+﻿--select *
+--from Genre;
+
+--SELECT * 
+--from Artist
+--ORDER BY ArtistName;
+
+--SELECT s.Title,
+--a.ArtistName
+--FROM Song s
+--Left Join Artist a on s.ArtistId = a.id;
+
+
+--SELECT art.ArtistName
+--FROM Artist art inner join 
+--	Album alb ON art.id = alb.ArtistId 
+--WHERE alb.GenreId = (SELECT id 
+--						FROM Genre
+--						WHERE label = 'Jazz');
+
+
+--SELECT art.ArtistName
+--FROM Artist art inner join 
+--	Album alb ON art.id = alb.ArtistId 
+--WHERE alb.GenreId in (SELECT id 
+--						FROM Genre
+--						WHERE label = 'Jazz' or label = 'Rock');
+
+
+
+--SELECT * from Album
+--WHERE AlbumLength = null;
+
+
+
