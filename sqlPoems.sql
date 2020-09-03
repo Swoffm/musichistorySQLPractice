17dd1ad476736c5bb4956b1767d605f4a9205b0c
Gavin Swofford <gavinswofford@gmail.com>
2020-09-03 10:58:39 -0500

    worked on poems practice

---
+++ sqlPoems.sql
@@ -0,0 +1,41 @@
+﻿
+SELECT * FROM Grade;
+
+
+
+SELECT * FROM PoemEmotion;
+
+SELECT COUNT(*) FROM Poem;
+
+
+SELECT * 
+FROM Author a inner join
+				Grade g on a.GradeId = g.Id INNER JOIN
+				Gender gen on a.GenderId = gen.Id
+			ORDER BY a.name;
+
+
+SELECT COUNT(p.WordCount) FROM Poem p;
+
+SELECT p.charcount, p.Title from Poem p
+					order by p.CharCount;
+
+
+SELECT Count(au.name) from Author au  
+	WHERE au.GradeId = (SELECT g.id FROM Grade g where g.Name = '3rd grade'); 
+
+SELECT Count(au.name) from Author au  
+	WHERE au.GradeId in (SELECT g.id FROM Grade g where g.Name = '1st grade' or g.name = '2nd grade' or g.name = '3rd grade'); 
+
+SELECT COUNT(p.authorID) FROM POEM p 
+					INNER JOIN Author au on p.AuthorId = au.Id 
+					WHERE au.gradeId = (SELECT g.id FROM Grade g where g.Name = '4th grade'); 
+
+
+		
+
+
+			
+
+
+
